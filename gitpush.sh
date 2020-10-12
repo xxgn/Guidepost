8167703052f47c327784a5cb9a944f621676810f
HNU-CSEE-Nuwa <hnucessanonym@outlook.com>
2020-10-12 16:25:11 +0800

    update job

---
+++ gitpush.sh
@@ -0,0 +1,19 @@
+###
+ # @Author: Firefly
+ # @Date: 2020-10-12 16:15:38
+ # @Descripttion: 
+ # @LastEditTime: 2020-10-12 16:24:52
+### 
+
+info = $1
+if [! $info]; then 
+    echo please input commit info
+else 
+    echo start to pull from origin main
+    git pull
+    # how to deal when occur error
+    echo start to push to origin main
+    git add .
+    git commit -m info
+    git push origin main
+fi
