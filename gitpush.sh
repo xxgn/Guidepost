70ed82c4246802a5595eb59d29895a6df6590d37
HNU-CSEE-Nuwa <hnucessanonym@outlook.com>
2020-10-12 16:35:24 +0800

    update sh

--- gitpush.sh
+++ gitpush.sh
@@ -2,16 +2,18 @@
  # @Author: Firefly
  # @Date: 2020-10-12 16:15:38
  # @Descripttion: 
- # @LastEditTime: 2020-10-12 16:26:12
+ # @LastEditTime: 2020-10-12 16:35:05
 ### 
 
-info = $1
-if [! $info]; then 
+# 接受所有参数作为 commit 的描述
+
+info=$*
+if [ ! $info ]; then 
     echo please input commit info
 else 
     echo start to pull from origin main
     git pull
-    # how to deal when occur error
+    # how to deal when occur error？
     echo start to push to origin main
     git add .
     git commit -m $info
