dc1bd4df413ae1e6e6b50403f7cac66da88bffdf
HNU-CSEE-Nuwa <hnucessanonym@outlook.com>
2020-10-12 16:26:37 +0800

    update gitpush sh

--- gitpush.sh
+++ gitpush.sh
@@ -2,7 +2,7 @@
  # @Author: Firefly
  # @Date: 2020-10-12 16:15:38
  # @Descripttion: 
- # @LastEditTime: 2020-10-12 16:24:52
+ # @LastEditTime: 2020-10-12 16:26:12
 ### 
 
 info = $1
@@ -14,6 +14,6 @@ else
     # how to deal when occur error
     echo start to push to origin main
     git add .
-    git commit -m info
+    git commit -m $info
     git push origin main
 fi
